512d5592cceb61f76c5da2d7cc03d4fae6663e8b
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2024-03-18 22:15:59 +0600

    added header part and blog

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
+
+import profile from '../../assets/images/profile.png';
+
+
 const Header = () => {
     return (
-        <div>
-            <h1 className='text-3xl font-bold'>Knowledge Cafe</h1>
-        </div>
+        <section className='container mx-auto'>
+            <div className=' flex justify-between mt-8 '>
+                <h1 className='text-4xl font-bold text-[#111111]'>Knowledge Cafe</h1>
+                <img src={profile} alt="" />
+            </div>
+            <div className='mt-6 border text-[#11111126] mb-8'></div>
+        </section>
+
+
     );
 };
+
+
 
 export default Header;
